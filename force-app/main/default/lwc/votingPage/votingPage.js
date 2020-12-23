6181e79b1f9a38b2d8b300ab7fcf36be98222c54
oliesnik <sasha.liesnik@gmail.com>
2020-12-23 12:40:11 +0200

    some css added

--- force-app/main/default/lwc/votingPage/votingPage.js
+++ force-app/main/default/lwc/votingPage/votingPage.js
@@ -4,10 +4,9 @@ import getCampaignList from '@salesforce/apex/VotingController.getCampaignList';
 import getNominationList from '@salesforce/apex/VotingController.getNominationList';
 import createVote from '@salesforce/apex/VotingController.createVote';
 import getContactList from '@salesforce/apex/VotingController.getContactList';
-// import getVoter from '@salesforce/apex/VotingController.getVoter';
 
 export default class VotingPage extends LightningElement {
-    queryTerm; 
+    queryTerm;
     contacts;
     contactsInNominations;
     nominations;
@@ -18,12 +17,12 @@ export default class VotingPage extends LightningElement {
     posibleVotes = {};
     updateContacts = false;
     hasVoted = false;
-    connectedCallback(){
-        getCampaignList().then(result => { 
+    connectedCallback() {
+        getCampaignList().then(result => {
             this.campaign = result;
-            getNominationList({campaign: this.campaign.Id}).then(result => { 
+            getNominationList({ campaign: this.campaign.Id }).then(result => {
                 this.nominations = result.slice();
-                getContactList({nominations: this.nominations}).then(result => { 
+                getContactList({ nominations: this.nominations }).then(result => {
                     this.contactsInNominations = result;
                     this.nominations.forEach(nominationItem => {
                         nominationItem.contacts = this.contactsInNominations[nominationItem.Id];
@@ -33,40 +32,15 @@ export default class VotingPage extends LightningElement {
             });
 
         });
-        
+
     }
 
-
-    // @wire(getContactList, {queryTerm:'$queryTerm', nominationsId: '$selectedNomination'})
-    // wiredContacts(response){
-    //     this.contacts = response.data;
-    // }
-    // @wire(getNominationList, {campaign: '$selectedCampaign'})
-    // wiredNominations(response){
-    //     this.nominations = response.data;        
-    // }
-    // @wire(getCampaignList)
-    // wiredCampaigns(response){
-    //     this.campaigns = response.data;
-    // }
-    handleEmailChange(evt){
+    handleEmailChange(evt) {
         this.voterEmail = evt.target.value;
     }
-    
-    // handleSearchChange(evt) {
-    //     this.queryTerm = evt.target.value;
-    // }
-
-    // handleNominationChange(event) {
-    //     this.selectedNomination = event.detail.value;
-    // }
-    
-    // handleCampaignChange(event) {
-    //     this.selectedCampaign = event.detail.value;
-    // }
 
     handleClickButton() {
-        if(!this.voterEmail){
+        if (!this.voterEmail) {
             this.dispatchEvent(
                 new ShowToastEvent({
                     title: 'Error',
@@ -74,32 +48,22 @@ export default class VotingPage extends LightningElement {
                     variant: 'error',
                 })
             );
-        }else{
-        // let voterContactId = getVoter({email: this.voterEmail});
-        // console.log('voterContact ' + voterContactId);
-        // console.log('selectedNomination ' + this.selectedNomination);
-        // console.log('selectedContact ' + this.selectedContact);
-        console.log('this.voterEmail ' + this.voterEmail);
-        createVote({finalVotes : this.posibleVotes, email: this.voterEmail})
-        .then( () => {
-            // this.dispatchEvent(
-            //     new ShowToastEvent({
-            //         title: 'Success',
-            //         message: 'Your vote has been submited',
-            //         variant: 'success',
-            //     })
-            // );
-            this.hasVoted = true;
-        })
-        .catch(error =>{ 
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error',
-                    message: error.body.message,
-                    variant: 'error',
+        } else {
+
+            console.log('this.voterEmail ' + this.voterEmail);
+            createVote({ finalVotes: this.posibleVotes, email: this.voterEmail })
+                .then(() => {
+                    this.hasVoted = true;
                 })
-            );
-        });
+                .catch(error => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: 'Error',
+                            message: error.body.message,
+                            variant: 'error',
+                        })
+                    );
+                });
         }
     }
 
@@ -108,26 +72,8 @@ export default class VotingPage extends LightningElement {
         let nomination = evt.currentTarget.dataset.id2;
         console.log('nomination: ' + nomination);
         console.log('this.selectedContact ' + selectedContact);
-        // this.posibleVotes.set(nomination, selectedContact);
         this.posibleVotes[nomination] = selectedContact;
-        // this.posibleVotes = {'nomination' : nomination , 'selectedContact' : selectedContact};
         console.log('this.posibleVotes: ', this.posibleVotes);
     }
 
-    // get nominationOptions() {
-    //     let listOfOptions = [];
-    //     this.nominations.forEach(nom => {
-    //         listOfOptions.push({ label: nom.Name, value: nom.Id });
-    //     });
-    //     return listOfOptions;
-    // }
-
-    // get campaignOptions() {
-    //     console.log('this.campaigns: ' + this.campaigns);
-    //     let listOfOptions = [];
-    //     this.campaigns.forEach(cam => {
-    //         listOfOptions.push({ label: cam.Name, value: cam.Id });
-    //     });
-    //     return listOfOptions;
-    // }
 }
